31b81afff55b9673ec4b5ff2bf7c1621e84b5994
Somoye Ayotunde <somoye.ayotunde@gmail.com>
2019-10-18 18:17:59 +0100

    completed openClassroom go fullstack course

---
+++ back-end/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
+const path = require('path');
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://somoye:XFgx5A3uhlLqGMl1@cluster0-jhfhr.mongodb.net/user?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+  })
+  .catch(error => {
+    console.log("Unable to connect to MongoDB Atlas!");
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+module.exports = app;
